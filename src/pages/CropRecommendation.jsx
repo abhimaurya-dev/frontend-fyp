5efa11dec00b917f9b78b486594751672c394682
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2023-05-01 11:48:24 +0530

    changed description of crop recommendation

--- src/pages/CropRecommendation.jsx
+++ src/pages/CropRecommendation.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Description from "../components/Description";
 
 const description =
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis hic explicabo sit consequatur, cum necessitatibus delectus tempore eveniet, quia ut distinctio dolor eius doloremque. Nisi sapiente facere quae obcaecati placeat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis hic explicabo sit consequatur, cum necessitatibus delectus tempore eveniet, quia ut distinctio dolor eius doloremque. Nisi sapiente facere quae obcaecati placeat.  ";
+  "Based on the N(Nitrogen % in Soil), P(Phosphorus % in Soil), K(Potassion % in Soil), temperature(in °C), humidity(gram per cubic metre), rainfall (per cubic centimetre), and ph, the model will recommend the optimum crop to grow on the given soil. Farming is related to real world,we no nothing about future but still we can predict it.So, If you want to predict your crop yield, you are at right place.We have developedthe software through which you can  predict the crop yield using machine learning. You have to just click the below button and fill the reqired data in the form.";
 
 const CropRecommendation = () => {
   return (
